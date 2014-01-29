459dcfb8480aaa6cd9b0b2decd66b66dc4ec5d4d
Vladimir Moravec <vmoravec@suse.com>
2014-01-29 17:45:34 +0100

    Add parameters for service search

--- src/lib/registration/service_discovery.rb
+++ src/lib/registration/service_discovery.rb
@@ -16,84 +16,123 @@
 # ServiceDiscovery.find :ntp
 ##
 
+# Prefix/URL scheme: service: [optional]
+# Service name: printer OR install.suse OR ntp
+# Protocol: lpr
+# Prefix + Service name => Abstract service type
+# Prefix + Service name + Protocol => Service Type
+# Attributes: attr1=>something, attr2=>something
+
 require 'yast'
 require 'resolv'
-require 'ipaddr'
+require 'ostruct'
 
 module Yast
   Yast.import 'SLP'
 
   module ServiceDiscovery
-    class << self
-      def find service_name, params={}
-        SlpService.find(service_name, params)
-      end
 
-      def find_all service_name, params={}
-        SlpService.all(service_name, params)
-      # find_slp_service(service_name, options[:scope]).map do |slp_service|
-      #   service = SlpService.new(options.merge(:name => service_name))
-      #   service.match?(slp_service) ? service : nil
-      # end
-      end
-
+    def self.find service_name, params={}
+      SlpService.find(service_name, params)
     end
 
-    # Prefix/URL scheme: service: [optional]
-    # Service name: printer OR install.suse OR ntp
-    # Protocol: lpr
-    # Prefix + Service name => Abstract service type
-    # Prefix + Service name + Protocol => Service Type
-    # Attributes: attr1=>something, attr2=>something
+    def self.find_all service_name, params={}
+      SlpService.all(service_name, params)
+    end
+
+    def self.list_types
+      SlpService.list_types
+    end
 
     class SlpService
-      SCHEME = 'service'
-      BASE_ATTRIBUTES = [ :name, :ip, :host, :protocol, :lifetime, :type ]
 
-      def self.find service_name, params
-        find_slp_service(service_name, params[:scope]).find do |slp_response|
-          service = new(service_name, slp_response)
-          service.verify!(params)
+      SCHEME = 'service'
+      DELIMITER = ':'
+
+      class << self
+        def find service_name, params
+          service = nil
+          slp_service_type = [SCHEME, service_name, params[:protocol]].compact.join(DELIMITER)
+          discover_slp_service(slp_service_type, params[:scope]).each do |slp_response|
+            service = new(service_name, slp_response, params)
+            service = service.match!(params)
+            break unless service.nil?
+          end
+          service
+        end
+
+        def all service_name, params
+          slp_service_type = [SCHEME, service_name, params[:protocol]].compact.join(DELIMITER)
+          discover_slp_service(slp_service_type, params[:scope]).map do |slp_response|
+            new(service_name, slp_response, params).match!(params)
+          end.compact
+        end
+
+        def list_types
+          available_services = []
+          discovered_services = discover_available_slp_service_types
+          return available_services if discovered_services.empty?
+
+          discovered_services.each do |slp_service_type|
+            available_services << parse_slp_type(slp_service_type)
+          end
+          available_services
+        end
+
+        private
+
+        def parse_slp_type service_type
+          type_parts = service_type.split(DELIMITER)
+          case type_parts.size
+          when 2
+            name = protocol = type_parts.last
+          when 3
+            name = type_parts[1]
+            protocol = type_parts[2]
+          else
+            raise "Incorrect slp service type: #{service.inspect}"
+          end
+          OpenStruct.new :name => name, :protocol => protocol
+        end
+
+        def discover_slp_service service_name, scope=''
+          SLP.FindSrvs(service_name, scope)
+        end
+
+        def discover_available_slp_service_types
+          SLP.FindSrvTypes('*', '')
         end
       end
 
-      def self.all service_name, params
-        slp_service_name = service_name
-        slp_service_name << ":#{params[:protocol]}" if params[:protocol]
-        find_slp_service(slp_service_name, params[:scope]).map do |slp_response|
-          service = create(service_name, slp_response, params)
-        end.compact
-      end
-
-      def self.create
-        # Return nil if params (which are expectations) do not meet the
-        # slp response. Otherwise return the new service
-      end
-
-      private
-
-      def self.parse_url url
-        url_parts = url.split(':')
-        url_parts.first == SCHEME ? url_parts : url_parts.unshift(SCHEME.dup)
-      end
-
-      def self.discover_slp_service service_name, scope=''
-        SLP.FindSrvs(service_name, scope)
-      end
-
-      attr_reader *BASE_ATTRIBUTES
+      attr_reader :name, :ip, :host, :protocol, :port, :url, :lifetime
+      attr_reader :slp_type, :attributes
 
       def initialize service_name, slp_data, params
         @name = service_name
         @ip = slp_data['ip']
-        @host = resolve_host
+        @port = slp_data['pcPort']
+        @slp_type = slp_data['pcSrvType']
         @lifetime = slp_data['lifetime']
-        @protocol = params[:protocol]
-        @url = NAME_PREFIX + name + protocol
-        @attributes = get_attributes
+        @url = slp_data['srvurl']
+        @protocol = params[:protocol] || slp_type.split(DELIMITER).last
+        @host = resolve_host
+        @attributes = OpenStruct.new(SLP.GetUnicastAttrMap(url, ip))
       end
 
-      def verify! params
+      def match! params
+        matches = []
+        params.each do |key, value|
+          if respond_to?(key)
+            result = send(key).to_s
+            matches << result.match(/#{value}/i)
+          elsif attributes.respond_to?(key)
+            result = attributes.send(key).to_s
+            matches << result.match(/#{value}/i)
+          else
+            matches << false
+          end
+        end
+        matches.all? ? self : nil
       end
 
       private
@@ -108,12 +147,7 @@ module Yast
       end
 
       def query_attributes
-        attributes = {}
-        SLP.UnicastFindSrvs(url, ip).each do |attribute|
-          attr_name, value = attribute.scan(/\A\((.+)\)/).flatten.first.split('=')
-          attributes[attr_name] = value
-        end
-        attributes
+        SLP.GetUnicastAttrMap(url,ip)
       end
     end
 
